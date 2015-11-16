cdb6e1d67429a75dcd5bd2c3774c0f5be0d670f3
Dmitriy Melnik <d.b.melnik@yandex.ru>
2015-11-16 11:39:15 +0600

    Создание формы создания заказа

---
+++ app/src/main/java/rt/dtech/kz/freight/activities/CreateOrderActivity.java
@@ -0,0 +1,27 @@
+package rt.dtech.kz.freight.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import rt.dtech.kz.freight.R;
+
+public class CreateOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_create_order);
+
+      fillCargoTypesSpinner();
+    }
+
+    private void fillCargoTypesSpinner() {
+      Spinner spinner = (Spinner) findViewById(R.id.cargoTypesSpinner);
+      ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+        this, R.array.cargoTypes, android.R.layout.simple_spinner_item);
+      adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+      spinner.setAdapter(adapter);
+    }
+}
