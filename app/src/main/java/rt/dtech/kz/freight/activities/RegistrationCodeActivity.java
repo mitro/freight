a97fe077b3e49a511b07d66ecbbe31c6979c5460
Dmitriy Melnik <d.b.melnik@yandex.ru>
2015-11-16 10:55:17 +0600

    Создание формы ввода кода регистрации

---
+++ app/src/main/java/rt/dtech/kz/freight/activities/RegistrationCodeActivity.java
@@ -0,0 +1,15 @@
+package rt.dtech.kz.freight.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import rt.dtech.kz.freight.R;
+
+public class RegistrationCodeActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_registration_code);
+  }
+}
